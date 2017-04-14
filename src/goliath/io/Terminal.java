8a2f0bdf451dcce66ca2499d730762709615ff86
ty <ty@ip-184-168-221-57.ip.secureserver.net>
2017-04-13 20:56:56 -0500

    Updated/Added license headers and changed to no longer handle IOException internally

--- src/goliath/io/Terminal.java
+++ src/goliath/io/Terminal.java
@@ -1,3 +1,26 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2017 Ty Young.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package goliath.io;
 
 import java.io.IOException;
@@ -23,16 +46,10 @@ public class Terminal
         workingDir = new File("./");
         commandString = null;
     }
-    public void startCommand()
+    public void startCommand() throws IOException
     {   
-        try
-        {
-            shell = Runtime.getRuntime().exec(commandString, null, workingDir);
-        }
-        catch (IOException ex)
-        {
-            Logger.getLogger(Terminal.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        shell = Runtime.getRuntime().exec(commandString, null, workingDir);
+        
         writer = new PrintWriter(shell.getOutputStream());
         reader = new Scanner(shell.getInputStream());
         errorReader = new Scanner(shell.getErrorStream()); 
